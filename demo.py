44b4d72a4674bf41ebcee22a15740beb662b2426
SachaDee <sachadehe@gmail.com>
2025-09-02 09:56:30 -0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -10,7 +10,13 @@ for img in imgs:
    print(f"treating: {img}")
    st = time.perf_counter()
    img = cv2.imread(f"./testImage/{img}")
-   res = face_system.recognize(img,k=5,threshold=0.46)
+   res = face_system.recognize(img,k=5,threshold=0.46) #5 results with the treshold (if available)
    end = time.perf_counter() - st
    print("inf Time face_system:",end)
-   print(res,"\n") 
+   for r in res:
+     print(r,"\n")
+   print(f"\n*****\n")
+
+####Now you can play create your own vector Database in recognizer.py
+####add_face and recognize or match
+####I will add more examples
